143327d2796957e373c32b8d52ed3bb923b3f190
Rheon Mais <rheonmais@gmail.com>
2022-12-11 02:01:27 +0000

    first implementation

--- src/TimerForm.js
+++ src/TimerForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import {HMSTosec} from './tools'
+import {HMSTosec, getTimeFormated} from './tools'
 import './TimerForm.css'
 
 
@@ -11,20 +11,22 @@ class TimerForm extends Component {
     this.setPricePerinterval = props.setPricePerinterval
     this.setSecondsPerPayment = props.setSecondsPerPayment
 
-    this.time = {
-      h: 1,
-      m: 0,
-      s: 0,
+    this.state = {
+      time: {
+        h: 1,
+        m: 0,
+        s: 0,
+      },
+      pricePerInterval: 10
     }
-    this.pricePerInterval = 10
   }
 
   validateInput() {
 
     const HMSFloor = {}
-    Object.entries(this.time).map(([key, value]) => HMSFloor[key] = Math.floor(value))
+    Object.entries(this.state.time).map(([key, value]) => HMSFloor[key] = Math.floor(value))
     // if no values have been entered stop method
-    if ((HMSFloor.h <= 0 && HMSFloor.m <= 0 && HMSFloor.s <= 0) || this.pricePerInterval <= 0) {
+    if ((HMSFloor.h <= 0 && HMSFloor.m <= 0 && HMSFloor.s <= 0) || this.state.pricePerInterval <= 0) {
       alert("format error")
       return
     }
@@ -32,43 +34,36 @@ class TimerForm extends Component {
     const timeSeconds = HMSTosec(HMSFloor)
 
     this.props.setSecondsPerPayment(timeSeconds)
-    this.props.setPricePerinterval(this.pricePerInterval)
+    this.props.setPricePerinterval(this.state.pricePerInterval)
     this.props.setConfirmed(true)
+  }
+
+  componentDidMount() {
+
   }
 
   render() {
     return (
       <div className='time-form'>
+        <p>{`£${this.state.pricePerInterval}/${getTimeFormated(HMSTosec(this.state.time), false)}`}</p>
         <div className='time-fields'>
-          <div className='spread'>
-            <p>h:   </p>
-            <input type='number' min={0} placeholder={this.time.h} onChange={event => this.time.h = event.target.value}></input>
-          </div>
-          <div className='spread'>
-            <p>m:   </p>
-            <input type='number' min={0} placeholder={this.time.m} onChange={event => this.time.m = event.target.value}></input>
-          </div>
-          <div className='spread'>
-            <p>s:   </p>
-            <input type='number' min={0} placeholder={this.time.s} onChange={event => this.time.s = event.target.value}></input>
-          </div>
-        <br />
-        <hr />
-        </div>
-        <br />
-        <div>
-          <p>price:   </p>
-          <br />  
-          <input type='number' min={0} placeholder={this.pricePerInterval} onChange={event => this.pricePerInterval = event.target.value}></input>
-          <br />
-          <br />
-          <br />
-
+          <p>£ </p>
+          <input className="input-price" type={'number'} min={0} defaultValue={this.state.pricePerInterval} onChange={event => this.setState({pricePerInterval: event.target.value})}></input>
+          <p> every </p>
+          <input type='time'
+            onChange={ value => {
+              console.log(value.target.value)
+              const [h, m] = value.target.value.split(':')
+              this.setState({time:
+                {h:h,
+                m:m,
+                s:0,}}
+              )
+            }}
+            defaultValue={`${this.state.time.h.toString().padStart(2, "0")}:${this.state.time.m.toString().padStart(2, "0")}:${this.state.time.s.toString().padStart(2, "0")}`}></input>
+          
           <hr />
         </div>
-        
-        <br />
-        <br />
         <button className='open-timer' onClick={() => this.validateInput()}>Open Timer</button>
       </div>
     )
